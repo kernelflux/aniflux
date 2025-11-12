c0e9b758e24c9cde9bcc227516efff58f2d2db2a
kernelflux <ktappdev2024@gmail.com>
2025-11-12 19:17:08 +0800

    opt pag

--- aniflux/build.gradle.kts
+++ aniflux/build.gradle.kts
@@ -33,15 +33,20 @@ android {
 
 //noinspection NewerVersionAvailable,UseTomlInstead
 dependencies {
+    api(fileTree("libs") {
+        include("*.jar", "*.aar")
+    })
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.constraintlayout)
 
+    api("androidx.exifinterface:exifinterface:1.4.1")
     api("com.squareup.okhttp3:okhttp:5.1.0")
-    api("com.tencent.tav:libpag:4.5.2")
     api("com.airbnb.android:lottie:6.7.1")
+
+    //api("com.tencent.tav:libpag:4.5.2")
     //api("io.github.tencent:vap:2.0.28")
     //api("pl.droidsonroids.gif:android-gif-drawable:1.2.29")
     //api("com.github.yyued:svgaplayer-android:2.6.1")
